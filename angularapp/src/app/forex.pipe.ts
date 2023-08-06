5fdab85983b957a2e055911c8ba113dcbe143cab
Iamneo <techadmin@iamneo.ai>
2023-08-06 08:51:03 +0000

    Backup Commit

--- angularapp/src/app/forex.pipe.ts
+++ angularapp/src/app/forex.pipe.ts
@@ -21,3 +21,22 @@ export class ForexPipe implements PipeTransform {
     return (amount / fromRate) * toRate;
   }
 }
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+const forexRates = {
+  USD: 1.126735,
+  GBP: 0.876893,
+  INR: 79.677056,
+};
+
+@Pipe({
+  name: 'forexConversion'
+})
+export class ForexConversionPipe implements PipeTransform {
+  transform(amount: number, fromCurrency: string, toCurrency: string): number {
+    const fromRate = forexRates[fromCurrency] || 1;
+    const toRate = forexRates[toCurrency] || 1;
+    return (amount / fromRate) * toRate;
+  }
+}
