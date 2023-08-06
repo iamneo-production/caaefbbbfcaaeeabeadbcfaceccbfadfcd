ee4211edb17b8cbd5b83469af1a8886ee2629512
Iamneo <techadmin@iamneo.ai>
2023-08-06 08:10:11 +0000

    Backup Commit

--- angularapp/src/app/app.component.spec.ts
+++ angularapp/src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'forex-converter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('forex-converter');
   });
 
   it('should render title', () => {
--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -23,9 +23,9 @@ export class AppComponent {
   convertedAmount: number = 0;
 
   forexRates: { id: string, value: number }[] = [
-    { "id": "USD", "value": 1.126735 },
-    { "id": "GBP", "value": 0.876893 },
-    { "id": "INR", "value": 79.677056 }
+    { "id":"USD",  "value": 1.126735},
+    { "id":"GBP", "value":0.876893},
+    { "id":"INR", "value":79.677056}
   ];
 
   convertForex() {
