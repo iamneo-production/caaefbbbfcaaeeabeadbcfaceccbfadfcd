7bab957d7b85c15b9dfce4d82170251227ce732a
Iamneo <techadmin@iamneo.ai>
2023-08-11 14:26:53 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -9,10 +9,18 @@ export class AppComponent {
   fromCurrency: string = 'USD';
   toCurrency: string = 'USD';
   amount: number = 0;
+  convertedAmount: number = 0; // Add convertedAmount property
 
   forexRates: { id: string, value: number }[] = [
     { "id": "USD", "value": 1.126735 },
     { "id": "GBP", "value": 0.876893 },
     { "id": "INR", "value": 79.677056 }
   ];
+
+  convertForex() {
+    const fromRate = this.forexRates.find(rate => rate.id === this.fromCurrency)?.value || 1;
+    const toRate = this.forexRates.find(rate => rate.id === this.toCurrency)?.value || 1;
+
+    this.convertedAmount = (this.amount / fromRate) * toRate;
+  }
 }
